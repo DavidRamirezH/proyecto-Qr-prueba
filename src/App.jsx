4874dfb2618de758b81a8856b96f212d6b7491c4
unknown <daviherrera18@gmail.com>
2023-12-23 23:03:12 -0500

    respo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 // app.js
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 import Footer from "./components/footer/footer";
 import Encabezado from "./components/encabezado/encabezado";
+
 // actualizado
 export default function App() {
   return(
